8a7af2af183a7c6ff83d05d8289e6f35c04ad642
JaumeCurruB51 <jamatelf@gmail.com>
2017-04-05 19:11:15 +0200

    RUN: minor changes

--- run.bash
+++ run.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-/bin/cp -a inventario/${1} ~/.ssh/config
-/bin/chmod 400 inventario/llaves/*
-mkdir PET
-echo ${1}
+/bin/cp -a inventario/${1} ~/.ssh/config || exit 1
+/bin/chmod 400 inventario/llaves/* || exit 1
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r cloud-user@dcip-testing:~ inventario
