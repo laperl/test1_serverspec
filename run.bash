075daa1f2d604d52a380113dd1b1c6dd6e763213
JaumeCurruB51 <jamatelf@gmail.com>
2017-04-06 19:37:45 +0200

    RUN: minor changes

--- run.bash
+++ run.bash
@@ -4,5 +4,6 @@
 /bin/chmod 400 inventario/llaves/* || exit 1
 #scp -i inventario/llaves/maquinatest.dcip_epg_ost.cloud.key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r inventario cloud-user@dcip-testing:~
 cd config
+ls -lR ~/.s*
 ssh -v dcip-testing
 rake spec
