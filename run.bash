a95d8b23acdaf2ac04ed0690b1aa2fd2abe858c5
JaumeCurruB51 <jamatelf@gmail.com>
2017-04-07 11:28:55 +0200

    RUN: minor changes

--- run.bash
+++ run.bash
@@ -4,8 +4,9 @@
 /bin/chmod 400 inventario/llaves/* || exit 1
 #scp -i inventario/llaves/maquinatest.dcip_epg_ost.cloud.key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r inventario cloud-user@dcip-testing:~
 #scp -i inventario/llaves/maquinatest.dcip_epg_ost.cloud.key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -rv inventario dcip-testing:~
-ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
-ssh-agent /bin/bash
+#ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
+eval `ssh-agent -s`
+#ssh-agent /bin/bash
 ssh-add inventario/llaves/maquinatest.dcip_epg_ost.cloud.key
 cd config
 rake spec
