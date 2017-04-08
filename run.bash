3664eee9b169adb5054e4508822e0b9fdcee6fd0
JaumeCurruB51 <jamatelf@gmail.com>
2017-04-08 15:58:05 +0200

    RUN: minor changes

--- run.bash
+++ run.bash
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-pwd
-/bin/cp inventario/${1} ~/.ssh/config || exit 1
+SSHCONFIGFILE=${1}
+SSHCONFIGFILE=${2}
+ENTORNO=${3}
+USUARIO=${4}
+CERTIFICADO=${5}
+IPDESTINO=${6}
+
+/bin/cp inventario/${SSHCONFIGFILE} ~/.ssh/config || exit 1
 /bin/cp inventario/llaves/* ~/.ssh/ || exit 1
 /bin/chmod 400 ~/.ssh/*key || exit 1
 #scp -i inventario/llaves/maquinatest.dcip_epg_ost.cloud.key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r inventario cloud-user@dcip-testing:~
 #scp -i inventario/llaves/maquinatest.dcip_epg_ost.cloud.key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -rv inventario dcip-testing:~
-#ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
 #eval `ssh-agent -s`
 #ssh-add inventario/llaves/maquinatest.dcip_epg_ost.cloud.key
-cd config
+cd config/${ENTORNO}
 rake spec
+
