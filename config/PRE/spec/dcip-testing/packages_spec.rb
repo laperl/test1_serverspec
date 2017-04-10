6fbd4c5630a0fcce4057c8571746adecf034a996
JaumeCurruB51 <jamatelf@gmail.com>
2017-04-10 16:07:02 +0200

    RUN: minor changes

--- config/PRE/spec/dcip-testing/packages_spec.rb
+++ config/PRE/spec/dcip-testing/packages_spec.rb
@@ -12,3 +12,8 @@ end
 describe port(22) do
   it { should be_listening }
 end
+
+describe default_gateway do
+      its(:ipaddress) { should eq '192.168.10.1' }
+        its(:interface) { should eq 'br0'          }
+end
